b0884f5cd5ca944ddf07201d100760cc1917d451
zhanyeye <zhanyeye@qq.com>
2019-05-17 20:27:57 +0800

    ItemTouchHelper & Appbar & Toolbar

--- myapplication08/src/main/java/com/example/myapplication08/MainActivity.java
+++ myapplication08/src/main/java/com/example/myapplication08/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication08;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        //动态修改toolBar属性
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitle("标题");
+        setSupportActionBar(toolbar);
+
+        //显示左箭头
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String msg = "";
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                msg = "home";
+                finish();
+                break;
+            case R.id.menu_add:
+                msg = "add";
+                break;
+            case R.id.menu_edit:
+                msg = "edit";
+                break;
+            case R.id.menu_send:
+                msg = "edit";
+                break;
+            case R.id.menu_del:
+                msg = "delete";
+                break;
+        }
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
